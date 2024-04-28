c47036a8903406d6ccef8e1fa5b1974bf36dc28a
Axel <axelb@AxelB>
2024-04-28 13:35:41 +0200

    changing/fixing input function and add Node and Edge as data-structures to use in the algorithm

--- EDAF05/labs/6railwayplanning/ccode/NF_alg.cc
+++ EDAF05/labs/6railwayplanning/ccode/NF_alg.cc
@@ -6,21 +6,42 @@ using std::cin;
 using std::endl;
 using std::string;
 
+struct Edge{
+    int index;
+    Node& nodeOne;              // nodeOne is the node with the lower index
+    Node& nodeTwo;
+    int currentCapacity;        // positiv capacity refers to flow from nodeOne to nodeTwo, negativ is opposite
+    int maximumCapacity;        // |currentCapacity| <= maximumCapacity
+
+    Edge(int i, Node& one, Node& two, int cap) : index(i), nodeOne(one), nodeTwo(two), currentCapacity(0), maximumCapacity(cap) {};
+
+    //int Edge::operator=(const Edge& newEdge){ *this = newEdge;};
+};
+
+struct Node{
+    int index;
+    std::vector<Edge&> connections;
+
+    Node(int i) : index(i) {};
+};
+
 int main(){
 
     int numOfNodes;
     int numOfEdges;
-    int capacity;
+    int goalCapacity;
     int numToRemove;
 
     cin >> numOfNodes;
     cin >> numOfEdges;
-    cin >> capacity;
+    cin >> goalCapacity;
     cin >> numToRemove;
 
-    std::vector<std::vector<int>> capacityMatrix(numOfNodes,std::vector<int>(numOfNodes, -1));
-    std::vector<std::pair<int,int>> plan(numOfEdges);
+    std::vector<Node> vecOfNodes(numOfNodes);
+    std::vector<Edge> vecOfEdges(numOfEdges);
     std::vector<int> planToRemove(numToRemove);
+
+    for(int i = 0; i < numOfNodes; i++){ vecOfNodes[i].index = i;}      // sets the index of all nodes, maybe not necassery.
 
     for(int i = 0; i < numOfEdges; i++){
         int startNode;
@@ -31,14 +52,24 @@ int main(){
         cin >> endNode;
         cin >> edgeCapacity;
 
-        capacityMatrix[startNode][endNode] = edgeCapacity;
-        capacityMatrix[endNode][startNode] = edgeCapacity;
-        plan[i] = std::pair<int,int>(startNode,endNode);
+        if(startNode > endNode){        // If the nodes are in the "wrong" order
+            int temp = startNode;
+            startNode = endNode;
+            endNode = temp;
+        }
+
+        Edge& newEdge = vecOfEdges[i];                          // setting the index of the edge
+        newEdge.nodeOne = vecOfNodes[startNode];                // setting the first node of the edge
+        newEdge.nodeTwo = vecOfNodes[endNode];                  // setting the second node of the edge
+        newEdge.maximumCapacity = edgeCapacity;                 // setting the maxcapacity of the edge
+        vecOfNodes[startNode].connections.push_back(newEdge);   // adding the edge to theier coresponding nodes 
+        vecOfNodes[endNode].connections.push_back(newEdge);     // adding the edge to theier coresponding nodes 
     }
 
     for(int i = 0; i < numToRemove; i++){
         int indexToRemove;
-        planToRemove[i] = indexToRemove;
+        cin >> indexToRemove;
+        planToRemove[i] = indexToRemove;                        // geting all the egeds and theier order of removal
     }
 
 
